1b8d964125dc1f8f9d4659adf24ad442cac6e2c0
ritesh8380 <pritesh8380@gmail.com>
2025-01-12 13:28:11 +0530

    twinkle twinkle little star with python

--- first.py
+++ first.py
@@ -1,1 +1,21 @@
-print("Hello guys lets begin the Python Ride")
+print("Hello guys lets begin the Python Ride\n")
+print("\t\t Twinkle-Twinkle little star \t\t")
+print("its too much time consuming")
+
+# Just use triple single quote or double quote 
+# congrats now you can write essays also
+
+print('''
+Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle little star
+How I wonder what you are
+When the blazing sun is gone
+When he nothing shines upon
+Then you show your little light
+Twinkle, twinkle, all the night
+Twinkle, twinkle, little star
+How I wonder what you are
+ ''')
