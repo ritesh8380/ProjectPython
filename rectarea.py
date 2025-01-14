04f0d995091b048450678171253ed095cf071658
ritesh8380 <pritesh8380@gmail.com>
2025-01-14 20:41:29 +0530

    Shop project will calculate the total price

---
+++ rectarea.py
@@ -0,0 +1,6 @@
+#will be using input() function to get the values from the user
+
+breadth = int(input("what is the breadth of rectangle ?\t"))
+length = int(input("what is the length of the rectangle ?\t"))
+area = length*breadth
+print(f"The area of rectangle is {area}")
