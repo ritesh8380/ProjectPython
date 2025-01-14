a5cfdfd206b6dfef074eec9a65832bdda087f538
ritesh8380 <pritesh8380@gmail.com>
2025-01-14 13:12:57 +0530

    Picture abhi baaki hai

--- first.py
+++ first.py
@@ -1,21 +1,5 @@
-print("Hello guys lets begin the Python Ride\n")
-print("\t\t Twinkle-Twinkle little star \t\t")
-print("its too much time consuming")
-
-# Just use triple single quote or double quote 
-# congrats now you can write essays also
-
-print('''
-Twinkle, twinkle, little star
-How I wonder what you are
-Up above the world so high
-Like a diamond in the sky
-Twinkle, twinkle little star
-How I wonder what you are
-When the blazing sun is gone
-When he nothing shines upon
-Then you show your little light
-Twinkle, twinkle, all the night
-Twinkle, twinkle, little star
-How I wonder what you are
- ''')
+# This is my very first programme in python
+print("i love kheer and puri\n")
+print("only Kheeer puri or 'puran-poli' have more attraction after coding")
+Date ='14-oct-2024'
+print(f"This the date of starting {Date}")
