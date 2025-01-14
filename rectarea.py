0ced6ac80a2cd2687760ea8414f33c11fbeeefc7
ritesh8380 <pritesh8380@gmail.com>
2025-01-14 20:49:45 +0530

    To calculate the area of rectangle

--- rectarea.py
+++ rectarea.py
@@ -3,4 +3,4 @@
 breadth = int(input("what is the breadth of rectangle ?\t"))
 length = int(input("what is the length of the rectangle ?\t"))
 area = length*breadth
-print(f"The area of rectangle is {area}")
+print(f"The area of rectangle is {area}" )
