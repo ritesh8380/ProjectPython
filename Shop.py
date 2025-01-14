f1e5cd9e2ae08a446eb15b15144f8c76b9b53d13
ritesh8380 <pritesh8380@gmail.com>
2025-01-14 20:46:44 +0530

    The programme to calculate total of commodity price

--- Shop.py
+++ Shop.py
@@ -2,5 +2,5 @@ product = input("Which commodity do you want ? :\t")
 Price = float(input("Price of the commodity ? :\t"))
 count = int(input("The quantity of commdity ? :\t"))
 Total = Price * count
-print(f"The price of {count} {product} is {Total} rupees ")
+print(f"The price of {count} {product} is {Total} rupees")
 
