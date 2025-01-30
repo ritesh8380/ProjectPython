904429d25a73d11e0c120dfe08b6e8268dcbb390
ritesh8380 <pritesh8380@gmail.com>
2025-01-30 19:17:30 +0530

    Timer using for loops

--- for_loops.py
+++ for_loops.py
@@ -9,4 +9,4 @@ for x in range(count,0,-1) :
     #To convert the seconds in hours we devide the seconds with 3600 
     print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
-print("TIME'S UP!") 
+print("TIME'S UP!")
