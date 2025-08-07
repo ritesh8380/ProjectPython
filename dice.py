6a1e2856bb9c82e8395f910508c97cf0c0eafddc
ritesh8380 <pritesh8380@gmail.com>
2025-08-07 11:31:01 +0530

    Dice rolling programme

---
+++ dice.py
@@ -0,0 +1,64 @@
+import random
+
+dice_art = { #dictionary as well as tuple is initialized
+    1:(  "┌──────────┐",
+         "│          │",
+         "│    ●     │",
+         "│          │",
+         "│          │",
+         "└──────────┘" ),
+    2:(
+         "┌──────────┐",
+         "│ ●        │",
+         "│          │",
+         "│          │",
+         "│        ● │",
+         "└──────────┘"),
+    3:( 
+         "┌──────────┐",
+         "│  ●       │",
+         "│    ●     │",
+         "│       ●  │",
+         "│          │",
+         "└──────────┘"),
+    4:(
+         "┌──────────┐",
+         "│  ●   ●   │",
+         "│          │",
+         "│  ●   ●   │",
+         "│          │",
+         "└──────────┘"),
+    5:(
+         "┌──────────┐",
+         "│ ●      ● │",
+         "│     ●    │",
+         "│          │",
+         "│  ●     ● │",
+         "└──────────┘"),
+    6:(  "┌──────────┐",
+         "│          │",
+         "│ ●      ● │",
+         "│ ●      ● │",
+         "│ ●      ● │",
+         "└──────────┘")
+} #dictionary has been initialized 
+
+dice = [] #list of dice has been created
+total = 0
+num_of_dice = int(input("Tell me the no. of dice : "))
+
+for die in range(num_of_dice) : #python dont know the grammer die or dim or even ritesh will also considered
+    dice.append(random.randint(1,6))
+
+'''for die in range(num_of_dice) :
+    for each_tuple_element in dice_art.get(dice[die]): #only access values 
+        print(each_tuple_element)'''
+
+for each_line_tuple in range(6) :
+    for die in dice :
+        print(dice_art.get(die)[each_line_tuple],end="") 
+    print()
+for die in dice :
+    total += die #sum up every die values  
+print(f"Total : {total}")
+
