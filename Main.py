4367ea1ee3f4c4305f6f4bd6cf841fb7e9405791
ritesh8380 <pritesh8380@gmail.com>
2025-08-13 13:41:14 +0530

    so this file will teach you how to use your own modules and informations about available modules

---
+++ Main.py
@@ -0,0 +1,13 @@
+# import example #My modified or my self made module imports
+#import example as E #Now you can use E instead of example module
+from example import Square #We can import many files and there fuctions at a time
+from math import pi
+
+print(pi)
+print(Square(2))
+#print(E.circum(2))
+
+'''Make normal python file and create few functions in that thats it now you can import them anywhere !'''
+print("-------------------------------------------------------------")
+print()
+print(help("modules"))
