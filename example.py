5046c1ae274ab58c8cec6216fb18d593a67e7b05
ritesh8380 <pritesh8380@gmail.com>
2025-08-13 13:47:22 +0530

    supporting module for the main.py file

---
+++ example.py
@@ -0,0 +1,12 @@
+def circum(radius) :
+    pi = 3.14
+    return 2*pi*radius
+
+def Areasquare(side) :
+    return side*side
+
+def Square(num) :
+    return num ** 2
+
+def Cube(num) :
+    return num ** 3 
