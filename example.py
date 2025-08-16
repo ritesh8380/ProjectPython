847ec7c62fae392713044bae25afad403936060a
ritesh8380 <pritesh8380@gmail.com>
2025-08-16 11:31:08 +0530

    updation in programme which contains fuctions and __name__

--- example.py
+++ example.py
@@ -11,5 +11,10 @@ def Square(num) :
 def Cube(num) :
     return num ** 3 
 
+Result = Cube(3) 
+print(f"Result")
+
 if __name__== "__main__" :
-    print("Finally you called me!")
+    print("Finally you called me!") 
+#So here __name__ is default created in python when you run the programme using python command the __name__ = "__main__"
+# but when you import the file or module __name__ = "file_name"
